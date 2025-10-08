e36d272e349ac8564df20e56ba2d1bedd4b98109
A.R <roziboyev614@gmail.com>
2025-10-08 16:10:58 +0500

    ikkinchi py fail

---
+++ ikkinchidars.py
@@ -0,0 +1,44 @@
+# Print bilan ishlash
+#print("Salom Barchaga aziz oquvchilar")
+#print('Hayr o\'quvchilar')
+#print("Hayr \n o'quv \n chilar")
+#print("Barchangiz ertangi darsga tayyorgarlik ko'rib kelinglar")
+#print("""string 
+#
+#print("""Dilmurod va 
+ #     Bozorvoy va 
+  #    Axmedjon""")
+
+# Pythonda Arfimetik amallar 
+
+# Qo'shish
+a = 12
+b = 18
+c = a + b
+print('c =', c)
+print(c)
+
+# Ayirish
+d = 12 + 18
+s = 25
+v = d - s
+print("v =", v)
+
+# Bo'lish
+
+zafarbek = 5
+dilmurod = 9
+print('Bolinuvchi(toliq) =', zafarbek/2)
+print("bolinuvchi(butunqismi) = ", zafarbek//2)
+
+# Kopaytirish
+n = 12
+l = 9
+p = n*l # 12 * 9
+u = n**l # 12*12*12*12......
+
+print('n(12)*l(9) =', p)
+print("n(12)**l(9) =", u)
+
+
+
