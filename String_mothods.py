d6963345d5ed6706c9384c8946ca3e595885e208
A.R <roziboyev614@gmail.com>
2025-10-20 16:13:16 +0500

    string methods va f"" dan foydalanish

---
+++ String_mothods.py
@@ -0,0 +1,29 @@
+# Pythonda String Methods va f"" dan foydalanish
+
+# mashina = 'depal S07'
+# narxi = 27000
+# rangi = 'qora'
+# kuchi = '200 ot kuchi'
+
+# print('Mening mashinam', mashina, '. Uning narxi', narxi, 'dollar. Rangi esa', rangi, '. Mashina kuchi esa', kuchi)
+# print(f"Mening mashinam {mashina}. Uning narxi {narxi} dollar. Rangi esa {rangi}. Mashina kuchi esa {kuchi}")
+
+
+# ism = 'zafarbek'
+# familiya = 'toxirov'
+# t_yil = 2015
+# yashash_joyi = 'qoraqosh qishlogi, zarafshon maxallasi, birinchi uy'
+# maktab = "qoraqosh umumta'lim maktabi number 1"
+# qiziqishlari = "kompyuter o'yinlari, futbol, dasturlash, kino korish"
+# qorquvlari = "yirtqich hayvonlar, yolgiz qolishdan, qorong'ulik"
+
+# print(f"asl : {ism}")
+# print(f"metod upper() : {ism.upper()}") # upper() metodi yordamida barcha harflarni katta harfga aylantiriladi
+
+# print(f"asl : {yashash_joyi}")
+# print(f"metod upper() : {yashash_joyi.upper()}") # upper() metodi yordamida barcha harflarni katta harfga aylantiriladi
+# print(f"metod capitalize() : {yashash_joyi.capitalize()}") # capitalize() metodi yordamida faqatgina birinchi harf katta harfga aylantiriladi
+# print(f"metod title() : {yashash_joyi.title()}") # title() metodi yordamida so'zlarning birinchi harfi katta harfga aylantiriladi
+
+lakatsiya = "XONQA TUMANI OLAJA QISHLOGI YANGI TURMUSH MAXALLASI"
+        #"Xonqa Tumani Olaja Qishlog'i Yangi Turmush Maxallasi"
---
+++ uchinchidars.py
@@ -0,0 +1,22 @@
+# Input() dan foydalanib Masala yechish
+
+#k_tomoni = int(input("Kvadratning tomonini kiriting: "))
+#b = input("B ga qiymat kiriting :")
+#print(type(b))
+
+# Kvadratning a tomoni berilgan uning Peremetirini toping
+#p = 4 * k_tomoni
+
+#print("Kvadratning Peremetri P = ", p) 
+
+# Kvadratning a va b tomoni berilgan uning Yuzi va Peremetirini toping
+
+a = int(input("Kvadratning a tomonini kiriting: "))
+b = int(input("Kvadratning b tomonini kiriting: "))
+
+p = 2 * (a + b)
+s = a * b
+
+print("Kvadratning Peremetri P = ", p) 
+print("Kvadratning Yuzi S = ", s)
+
