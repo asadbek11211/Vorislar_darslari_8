0c8590e072ffb0bc14540db54d6fe9d8df421862
A.R <roziboyev614@gmail.com>
2025-10-24 16:08:13 +0500

    Royhatlar bilan amaliyot

---
+++ Pythonda_Royhatlar(List).py
@@ -0,0 +1,130 @@
+# """
+# 22.10.2025
+# Pythonda royhatlar (List)
+
+# # """
+# # ism = 'Dilmurod'
+# # ism = 'Dilmurodbek'
+# # ism = 'Zafarbektoy'
+
+# # print(ism)
+
+# # ismlar = ['Dilmurod', 'Zafarbek', 'Abdulloh', 'Jasur', 'Muhammad']
+# # #           0              1         2              3       4
+# #  #          -5             -4       -3              -2      -1
+# # print(ismlar)
+
+# # mevalar = ["olma", "banan", "uzum","nok","behi","shaftoli","anor","o'rik","gilos","mandarin","apelsin","ananas"]
+# # moshinalar = ["bmw","mercedes","audi","ferrari","lamborgini","tesla","volvo","toyota","hyundai","kia"]
+
+# # print(moshinalar)
+# # print(moshinalar[4])
+# # print(mevalar[2].capitalize())
+# # print(mevalar[5].upper())
+# # print(ismlar[4])
+# # print(ismlar[-1].lower())
+# # print(mevalar[4].upper())
+# # print(moshinalar[-5].capitalize())
+
+# # hayvonlar = []
+# # print(hayvonlar)
+
+# # hayvonlar.append("ot")
+# # print(hayvonlar)
+
+# # hayvonlar.append("kalamush")
+# # print(hayvonlar)
+
+# # hayvonlar.append("tiger")
+# # print(hayvonlar)
+
+# # hayvonlar.append("_")
+# # print(hayvonlar)
+
+# # hayvonlar.append("mushuk")
+# # print(hayvonlar)
+
+# # hayvonlar.append("kurka")
+# # print(hayvonlar)
+
+# # hayvonlar.append("tongiz")
+# # print(hayvonlar)
+
+
+# # hayvonlar.append("eshshak")
+# # print(hayvonlar)
+
+
+# # hayvonlar.append("sigir")
+# # print(hayvonlar)
+
+# # hayvonlar.append("o`rdak")
+# # print(hayvonlar)
+
+# # hayvonlar.append("toshbaqa")
+# # print(hayvonlar)
+
+# # hayvonlar.append("ko`rshapalak")
+# # print(hayvonlar)
+
+# # hayvonlar.append("baliq")
+# # print(hayvonlar)
+
+# # hayvonlar.append("aqula")
+# # print(hayvonlar)
+
+# # hayvonlar.append("qisqichbaqa")
+# # print(hayvonlar)
+
+# # hayvonlar.append("")
+# # print(hayvonlar)
+
+# # hayvonlar.insert(0 , "buqa")
+# # print(hayvonlar)
+
+# # hayvonlar.insert(1 , "toshbaqa")
+# # print(hayvonlar)
+
+# moshinalar = ["bmw","mercedes","audi","ferrari","lamborgini","tesla","volvo","toyota","hyundai","kia"]
+# print(moshinalar)
+
+# moshinalar.remove("tesla")
+# print(moshinalar)
+
+# moshinalar.remove("kia")
+# print(moshinalar)
+
+# del moshinalar[0]
+# print(moshinalar)
+
+# del moshinalar[0]
+# print(moshinalar)
+
+# del moshinalar[3]
+# print(moshinalar)
+
+# del moshinalar[4]
+# print(moshinalar)
+
+# del moshinalar[0]
+# print(moshinalar)
+
+# moshinalar.append("bmw")
+# print(moshinalar)
+
+# moshinalar.append("merceders")
+# print(moshinalar)
+
+# moshinalar.insert(0 , "bmw")
+# print(moshinalar)
+
+# moshinalar.insert(1 , "merceders")
+# print(moshinalar)
+
+# ismlar = ["Muhammadjon", "Xursandjon", "Zafarjon"]
+# print(ismlar)
+
+# print
+# print(f"Salom dostim {ismlar[0]} uch soat oldin {ismlar[1]} uni korgandim. U aytdiki bugun {ismlar[2]} bizni uyida kutib turarkan")
+
+sonlar = [1, 2, 22 , 3,]
---
+++ Royhatlar_amaliyot.py
@@ -0,0 +1,35 @@
+# mashinalar = ["BMW", "Tayota", "Matiz"]
+# print(mashinalar)
+# mashinalar.append("Qoblt")
+# print(mashinalar)
+
+# mashinalar.append("Byd")
+# print(mashinalar) 
+
+# ismlar = ["zafarbek","muhammadjon"]
+# print(ismlar)
+ 
+# print(ismlar[0].capitalize())
+
+# print(ismlar[1].upper())
+
+# ismlar.insert(0,'bozorvoy')
+# print(ismlar)
+
+# ismlar.insert(2,'ahmetjon')
+# print(ismlar)
+
+# del ismlar[1]
+# print(ismlar)
+
+# ismlar.remove("muhammadjon")
+# print(ismlar)
+
+
+
+# print(mashinalar)
+
+# mashinalar.pop(3)
+# print(mashinalar)
+
+
