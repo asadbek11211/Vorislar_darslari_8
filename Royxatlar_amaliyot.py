6b843063d23ee963df40b76ec881c9608b137b8d
A.R <roziboyev614@gmail.com>
2025-10-29 16:16:48 +0500

    aada

---
+++ Royxatlar_amaliyot.py
@@ -0,0 +1,18 @@
+# davlatlar = ["O`zbekiston", "AQSh", "Rossiya", "Xitoy", "Germaniya", "Fransiya", "Italiya", "Ispaniya", "Kanada", "Avstraliya"]
+# print(davlatlar)
+
+# print("Davlatlar soni", len(davlatlar))
+
+# print("Asl nusxa :" ,davlatlar)
+# print("Teskari tartib : ",sorted(davlatlar, reverse=True))
+
+
+#sort() metodi yordamida ro'yxatni avval alifbo bo'yicha, keyin esa alifboga teskari tartibda konsolga chiqaring.
+# davlatlar.sort()
+# print(davlatlar)
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+
+sonlar = list(range(120,351))
+
+print(max(sonlar)-min(sonlar))
