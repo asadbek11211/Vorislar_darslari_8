3a7437937532afdb99d9037182b019b1e6d2675f
A.R <roziboyev614@gmail.com>
2025-12-03 16:54:51 +0500

    aa

---
+++ takrorlash.py
@@ -0,0 +1,29 @@
+# qiymat1=int(input("Qiymat kiriting "))
+# qiymat2=int(input("yana qiymat kiriting"))
+# kvadrt1=qiymat1**2
+# kvadat2=qiymat2**2
+# yigindi=kvadrt1+kvadat2
+# print("yigindi =",yigindi)
+# royhat=[1,5,8,7,5,4,5,48,4,8]
+# print(royhat[5])
+# print(royhat[0]+royhat[9])
+# royhat=list(range(30,51,1))
+# print(royhat)
+# qiymat1=int(input("Qiymat1 ni kiriting "))
+# qiymat2=int(input("Qiymat2 ni kiriting "))
+# qiymat3=int(input("Qiymat3 ni kiriting "))
+# kub1=qiymat1**3
+# kub2=qiymat2**3
+# kub3=qiymat3**3
+# yigindi=(kub1+kub2+kub3)
+# print(yigindi)
+mevalar=['olma','gilos','banan','orik']
+print(mevalar[2])
+mevalar.insert(2,'olcha')
+print(mevalar)
+
+mevalar.append('shaftoli')
+print(mevalar)
+
+mevalar.pop()
+print(mevalar)
