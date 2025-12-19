4bb4a992a8fd6aa87c7af2e80b5033d3e01910b3
A.R <roziboyev614@gmail.com>
2025-12-19 15:45:39 +0500

    sb

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,13 @@
-boburbek = {
-    'ism':'boburbek',
-    'yosh': 15,
-    'yolgonchi': False,
-    't_yil': 2010,
-    'tel_nomer': 976665544,
-    'manzil':'qora qosh'
-}
+# boburbek = {
+#     'ism':'boburbek',
+#     'yosh': 15,
+#     'yolgonchi': False,
+#     't_yil': 2010,
+#     'tel_nomer': 976665544,
+#     'manzil':'qora qosh'
+# }
 
-print(boburbek['ism'].title())
-print(boburbek['tel_nomer'])
-print(f"{boburbek['ism'].title()} ning tel nomeri {boburbek['tel_nomer']}")
+# print(boburbek['ism'].title())
+# print(boburbek['tel_nomer'])
+# print(f"{boburbek['ism'].title()} ning tel nomeri {boburbek['tel_nomer']}")
+math.floor(3.9) 
